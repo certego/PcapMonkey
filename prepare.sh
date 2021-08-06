9903623d63bee1da3de98b4428b91a532fd3e1bf
Hariom Chaturvedi <hariom4694@gmail.com>
2021-08-06 19:05:45 +0530

    Modified prepare.sh
    Signed-off-by: Hariom Chaturvedi <hariom4694@gmail.com>

--- prepare.sh
+++ prepare.sh
@@ -3,5 +3,5 @@
 sudo chown 1000:1000 ./config/filebeat/filebeat.yml 
 sudo chown -R 1000:1000 ./config/filebeat/modules.d/
 
-chmod 644 ./config/filebeat/filebeat.yml
-chmod 644 ./config/filebeat/modules.d/*
+sudo chmod 644 ./config/filebeat/filebeat.yml
+sudo chmod 644 ./config/filebeat/modules.d/*
