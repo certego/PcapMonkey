fbbf8c02504b4d323d402aa8ce9538a00444b7cd
Hariom Chaturvedi <hariom4694@gmail.com>
2021-08-06 19:03:35 +0530

    Modified Workflow, and prepare.sh
    Signed-off-by: Hariom Chaturvedi <hariom4694@gmail.com>

--- prepare.sh
+++ prepare.sh
@@ -1,3 +1,7 @@
 #!/bin/sh
 
+sudo chown 1000:1000 ./config/filebeat/filebeat.yml 
+sudo chown -R 1000:1000 ./config/filebeat/modules.d/
+
+chmod 644 ./config/filebeat/filebeat.yml
 chmod 644 ./config/filebeat/modules.d/*
