e6753121c29d38aae789b43eb75d68f8deefdd5e
Mayank Malik <mayankmalik012@gmail.com>
2021-11-16 23:29:28 +0530

    Added fix for Windows Docker Deployment Non Shared Volume error

--- prepare.sh
+++ prepare.sh
@@ -5,3 +5,7 @@ sudo chown -R 1000:1000 ./config/filebeat/modules.d/
 
 sudo chmod 644 ./config/filebeat/filebeat.yml
 sudo chmod 644 ./config/filebeat/modules.d/*
+
+if [[ "$1" == "win" ]]; then
+    sed -i -e 's/\/var\/lib\/suricata\/rules\//\/var\/lib\/suricata\/rules/g' ./docker-compose.yaml
+fi
